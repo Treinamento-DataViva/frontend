99d35cf01453b98bcb5b796092dd9f621ed71b99
jvvls <joaovictorvial@gmail.com>
2026-03-20 14:14:01 -0300

    criação da pagina dashboards

---
+++ frontend-censo-escolar/src/pages/Dashboard.tsx
@@ -0,0 +1,11 @@
+
+
+export default function Dashboard() {
+    return (
+        <div>
+            <h1>Dashboard</h1>
+            <p>Bem-vindo ao Dashboard do Censo Escolar!</p>
+            {/* Aqui você pode adicionar gráficos, tabelas e outras visualizações de dados */}
+        </div>
+    )
+}
